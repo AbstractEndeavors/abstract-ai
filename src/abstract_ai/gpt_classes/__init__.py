da2715e997f9cb29e34cf3535a8ec7baab1f63d5
putkoff <57512254+putkoff@users.noreply.github.com>
2023-10-30 16:52:01 -0500

    Delete src/abstract_ai directory

--- src/abstract_ai/gpt_classes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .api_selection.ApiBuilder import ApiManager
-from .model_selecion.ModelBuilder import ModelManager
-from .prompt_selection.PromptBuilder import PromptManager
-from .response_selection.ResponseBuilder import ResponseManager
-from .instruction_selection.InstructionBuilder import InstructionManager
-
